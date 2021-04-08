442f0372c78478050a3df5e2992169edc388bbfe
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-04-07 20:30:47 -0700

    Added javascript to fetch quotes from api

---
+++ script.js
@@ -0,0 +1,22 @@
+//Get Quotes From API
+let apiQuotes = [];
+//Show new quote
+function newQuote() {
+  //Pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // handle errors
+    console.log(error);
+  }
+}
+
+//On Load
+getQuotes();
